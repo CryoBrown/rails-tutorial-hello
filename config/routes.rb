364ec878a7aac3117c2d09206f43f5e7c1902c36
CryoBrown <ChrisBrownDev@gmail.com>
2016-06-27 18:16:17 +0000

    added redundnat hello route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
    root 'application#hello'
    
    get 'goodbye' => 'application#goodbye'
+   
+   get 'hello' => 'application#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
